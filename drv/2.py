9aed1000edbe317816a7e1e6423ee45f8502d376
Iskandar Gabdrakhmanov <faruk.iskandar@yandex.ru>
2016-02-10 12:01:58 +0300

    added new functionality to drv

--- drv/2.py
+++ drv/2.py
@@ -1,5 +1,26 @@
+import sys
 from fractions import Fraction
 from prettytable import PrettyTable
+# -file 'file_path'
+
+print(sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3])
+
+
+# parses the arguments passed to the script
+def parse_args(args):
+    if len(args) == 3:
+        return True
+    else:
+        return False
+
+
+# checks whether the args correct or not
+def check_parse_args_return(ret):
+    if ret is False:
+        print("Usage: script_name -file path_to_file")
+    else:
+        pass
+
 
 x = [[1, Fraction(2, 6)], [2, Fraction(1, 6)], [3, Fraction(3, 6)]]
 y = [[1, Fraction(1, 7)],[2, Fraction(2, 7)],[3, Fraction(4, 7)]]
